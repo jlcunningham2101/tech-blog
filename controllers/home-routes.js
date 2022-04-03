6b90062c1bfdae7a303695f84adb8ad89344a810
Jill Cunningham <jleighcunningham@gmail.com>
2022-04-03 16:54:16 -0400

    fixed typos in post-info handlebars and controllers

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,7 @@ const { Post, User, Comment } = require("../models");
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: ["id", "post_text", "title", "created_at"],
     include: [
       {
         model: Comment,
@@ -43,7 +43,7 @@ router.get("/post/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: ["id", "post_text", "title", "created_at"],
     include: [
       {
         model: Comment,
