b63db1c57a10cd89c83dc21a673e0ece52fa0fa6
Jill Cunningham <jleighcunningham@gmail.com>
2022-04-24 23:38:04 -0400

    updated typos and mismatches in handlebars

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,14 +1,14 @@
 async function newFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
-  const post_text = document.querySelector('input[name="post-text"]').value;
+  const postTitle = document.querySelector('input[name="post-title"]').value;
+  const contents = document.querySelector('input[name="post-text"]').value;
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
     body: JSON.stringify({
-      title,
-      post_text,
+      postTitle,
+      contents,
     }),
     headers: {
       "Content-Type": "application/json",
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,32 +2,32 @@ const { Post } = require("../models");
 
 const postdata = [
   {
-    title: "Tech Trends",
-    post_text:
+    postTitle: "Tech Trends",
+    contents:
       "One of the best examples of tech trends blogs we have seen is from Tulip, the Internet of Things (IoT) developer for industrial manufacturers. The startup raised $40 million in funding back in 2017 and not long after created one of the best tech blogs around. It’s been attracting steady organic traffic ever since.",
     user_id: 1,
   },
   {
-    title: "Tutorials and Guides",
-    post_text:
+    postTitle: "Tutorials and Guides",
+    contents:
       "Salesforces “A Beginners Guide to CRM Systems” on their blog attracts over 10,000 organic site visitors per month, according to the SEO tool, Ahrefs.",
     user_id: 2,
   },
   {
-    title: "Security Tips",
-    post_text:
+    postTitle: "Security Tips",
+    contents:
       "In 2020, companies around the world are set to spend over $173 billion on cybersecurity. Not to mention, there is a hacker attack every 39 seconds on someones personal computer or phone. Understandably, companies and individuals are worried about cybersecurity now more than ever.",
     user_id: 3,
   },
   {
-    title: "FAQs",
-    post_text:
+    postTitle: "FAQs",
+    contents:
       "The complexity of the tech industry leads to a lot of unanswered questions and confused customers. So why not answer some of the most commonly asked questions in full-length blog posts? It could save you some time emailing back and forth with clients. Plus, FAQs are great for web traffic.",
     user_id: 4,
   },
   {
-    title: "Highlight Reviews",
-    post_text:
+    postTitle: "Highlight Reviews",
+    contents:
       "If your business is in a crowded, competitive space in the tech industry, it might pay to write blogs that highlight positive reviews. This is a tactic that Jiobit, a manufacturer of comfortable health sensors for kids, uses regularly.",
     user_id: 5,
   },
